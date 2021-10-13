19a3949d4f9d140d4eb55f31075f3f9495e6a9ca
渭河 <weihe.whq@alibaba-inc.com>
2021-10-13 14:32:10 +0800

    增加发布脚本

---
+++ release.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import os,sys
+
+specs = ['AliInteractiveDocumentCore.podspec','AliInteractiveLiveCore.podspec','AliInteractiveRTCCore.podspec','AliInteractiveRoomBundle.podspec','AliInteractiveVideoPlayerCore.podspec','AliInteractiveWhiteBoardCore.podspec','AliStandardLiveRoomBundle.podspec']
+
+if len(sys.argv) == 0:
+    print('please input version')
+else:
+    version = sys.argv[1]
+
+    for spec in specs:
+    	with open(spec) as f:
+            lines = f.readlines()
+            for i in range(len(lines)):
+                if lines[i].find('s.version      =') != -1:
+                    lines[i] = "  s.version      = \"" + version + "\"" + "\n"
+        f.close
+        
+
+        with open(spec,'w+') as wf:
+            wf.writelines(lines)
+            wf.close
+
+
+    tag = "git tag -a "+version+" -m '"+version+"'"
+    trunk = 'pod trunk push ' + spec + ' --allow-warnings --verbose --skip-import-validation'
+
+   # os.system('git add .')
+   # os.system('git commit -m release')
+   # os.system('git push')
+   # os.system(tag)
+   # os.system('git push --tags')
+   # os.system(trunk)
+    
+
+    
+    
